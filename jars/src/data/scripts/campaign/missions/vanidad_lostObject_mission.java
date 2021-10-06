bb03e5a560c5baa45717fedb94122807105e65c4
Fabrice Valade <fabrice.valade@neoculus.com>
2021-10-06 13:53:30 -0400

    finished pipework for the quest!

--- jars/src/data/scripts/campaign/missions/vanidad_lostObject_mission.java
+++ jars/src/data/scripts/campaign/missions/vanidad_lostObject_mission.java
@@ -10,11 +10,17 @@ import com.fs.starfarer.api.campaign.CustomCampaignEntityAPI;
 import java.awt.Color;
 
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.BattleAPI;
+import com.fs.starfarer.api.campaign.CampaignEventListener;
 import com.fs.starfarer.api.campaign.FactionAPI;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.PersonImportance;
 import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.campaign.StarSystemAPI;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.campaign.listeners.BaseFleetEventListener;
+import com.fs.starfarer.api.campaign.listeners.FleetEventListener;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
 import com.fs.starfarer.api.characters.PersonAPI;
 import com.fs.starfarer.api.impl.campaign.DerelictShipEntityPlugin;
 import com.fs.starfarer.api.impl.campaign.ids.Ranks;
@@ -22,170 +28,199 @@ import com.fs.starfarer.api.impl.campaign.ids.Tags;
 import com.fs.starfarer.api.impl.campaign.missions.hub.BaseHubMission;
 import com.fs.starfarer.api.impl.campaign.missions.hub.HubMissionWithBarEvent;
 import com.fs.starfarer.api.impl.campaign.missions.hub.ReqMode;
-import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.special.ShipRecoverySpecial;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
 import com.fs.starfarer.api.util.WeightedRandomPicker;
 import java.util.Random;
 import com.fs.starfarer.api.impl.campaign.ids.FleetTypes;
+import com.fs.starfarer.api.impl.campaign.missions.hub.MissionTrigger;
+import com.fs.starfarer.api.impl.campaign.missions.hub.MissionTrigger.TriggerAction;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author Lethargie
  */
 
-
-
 public class vanidad_lostObject_mission extends HubMissionWithBarEvent {
-        
-        public static float SCAVENGER_WEIGHTED_PROB = 10f;
-        public static float AUTHORITY_WEIGHTED_PROB = 15f;
-        public static float SPACESHIP_PROBABILITY = 0.75f;
-	
-	public static enum Stage {
-		GOGET,
-                GET_IT_FROM_SCAVENGER,
-                GET_IT_WITH_AUTHORITY,
-		RETURN,
-		COMPLETED,
-		FAILED,
-	}
-	public static enum TargetVariation {
-            PlANETSIDE,
-            SPACESHIP
-        }
-        
-        public static enum Variation {
-            SCAVENGER,
-            AUTHORITY,
-            NOTHING,
-        }
-        
-        protected Variation variation;
-        protected TargetVariation targetVariation;
-	protected String targetObject;
-        protected PersonAPI missionGiver;
-        protected StarSystemAPI system;
-	protected SectorEntityToken entity;
-        protected String hullname, shipname;
-        
-        
+
+    public static float AUTHORITY_WEIGHTED_PROB = 15f;
+    public static float NOTHING_WEIGHTED_PROB = 10f;
+
+    public static enum Stage {
+        GOGET,
+        RETURN,
+        COMPLETED,
+        FAILED,
+        FAILED_DECIV
+    }
+
+
+
+    public static enum Variation {
+        SCAVENGER,
+        AUTHORITY,
+        NOTHING,
+    }
+
+    protected static String AUTHORITY_FLAG = "$vanidad_loe_authority";
+    protected static String HAS_PROTECTING_FLEET_FLAG = "$vanidad_loe_hasProtectingFleet";
+    protected static String DEFENDING_FLEET_DEFEAT_TRIGGER = "vanidad_loe_t_defendingFleetDefeated";
+    protected static String BRIBE_VARIABLE = "$vanidad_loe_authorityPayment";
+    
+    protected Variation variation;
+    protected String targetObject;
+    protected PersonAPI missionGiver;
+    protected StarSystemAPI system;
+    protected SectorEntityToken entity;
+    protected String hullname, shipname;
+    protected CampaignFleetAPI FleetBlockingEntity;
+
+    public class ObtainFleetBlockingEntity implements TriggerAction {
+
         @Override
-        public boolean shouldShowAtMarket(MarketAPI market) {
-            return market.getFactionId().equals("vanidad");
-	}
+        public void doAction(MissionTrigger.TriggerActionContext context) {
+
+            FleetBlockingEntity =  context.fleet;
+        }
+
+    }
+
+    @Override
+    public boolean shouldShowAtMarket(MarketAPI market) {
+        return market.getFactionId().equals("vanidad");
+    }
+
+    @Override
+    protected boolean create(MarketAPI createdAt, boolean barEvent) {
+        //genRandom = Misc.random;
+
+        //if (!"vanidad".equals(createdAt.getFaction().getId())) return false;
+        if (barEvent) {
+            setGiverRank(Ranks.CITIZEN);
+            setGiverPost(Ranks.POST_TRADER);
+            setGiverImportance(PersonImportance.LOW);
+            setGiverTags(Tags.CONTACT_UNDERWORLD);
+            setGiverFaction("vanidad_liberador");
+            findOrCreateGiver(createdAt, true, true);//put somone as personOveride;
+        }
+
+        missionGiver = getPerson();//either return the personOveride or the hub.getperson
+        if (missionGiver == null) {
+            return false;
+        }
+
+        if (!setPersonMissionRef(missionGiver, "$vanidad_loe_ref")) {
+            return false;
+        }//put something in memory? its confusing
+
+        targetObject = pickOne("a team operating as scout",
+                               "a team collecting experimental data",
+                               "a team establishing diplomatic contact",
+                               "a travelling executive"
+        );
+
+        requireSystemTags(ReqMode.ANY, Tags.THEME_DERELICT, Tags.THEME_MISC,
+                          Tags.THEME_RUINS);
+        requireSystemTags(ReqMode.NOT_ANY, Tags.THEME_REMNANT); //remove remnant systems
+        preferSystemUnexplored();
+        preferSystemInDirectionOfOtherMissions();
+
+        system = pickSystem();
+        if (system == null) {
+            return false;
+        }
+
+        WeightedRandomPicker<Variation> variationPicker = new WeightedRandomPicker<Variation>(
+                new Random());
+        variationPicker.add(Variation.NOTHING, NOTHING_WEIGHTED_PROB);
+        variationPicker.add(Variation.AUTHORITY, AUTHORITY_WEIGHTED_PROB);
+        variation = variationPicker.pick();
+
+        //variation = Variation.AUTHORITY;
+
+        pickTargetShip();
+        if (entity == null) {
+            return false;
+        }
+
+        setStartingStage(Stage.GOGET);
+        setSuccessStage(Stage.COMPLETED);
+        addFailureStages(Stage.FAILED);
+
+        makeImportant(entity, "$vanidad_loe_target", Stage.GOGET);
+        makeImportant(missionGiver, "$vanidad_loe_missionGiver", Stage.RETURN);
+        setEntityMissionRef(entity, "$vanidad_loe_ref");
         
-        
-	@Override
-	protected boolean create(MarketAPI createdAt, boolean barEvent) {
-		//genRandom = Misc.random;
+        connectWithMemoryFlag(Stage.GOGET, Stage.RETURN, entity,
+                              "$vanidad_loe_f_needToReturn");
+        setStageOnMemoryFlag(Stage.COMPLETED, missionGiver,
+                             "$vanidad_loe_f_completed");
+        addNoPenaltyFailureStages(Stage.FAILED_DECIV);
+	setStageOnMarketDecivilized(Stage.FAILED_DECIV, createdAt);
+        if (variation == Variation.AUTHORITY) {
+            entity.getMemoryWithoutUpdate().set(AUTHORITY_FLAG, true);
+            entity.getMemoryWithoutUpdate().set(HAS_PROTECTING_FLEET_FLAG, true);
+            setupFleetEncounter();
+        }
 
-		//if (!"vanidad".equals(createdAt.getFaction().getId())) return false;
-		
-		if (barEvent) {
-			setGiverRank(Ranks.CITIZEN);
-			setGiverPost(Ranks.POST_TRADER);
-			setGiverImportance(PersonImportance.LOW);
-			setGiverTags(Tags.CONTACT_UNDERWORLD);
-                        setGiverFaction("vanidad_liberador");
-			findOrCreateGiver(createdAt, true, true);//put somone as personOveride;
-		}
-		
-		missionGiver = getPerson();//either return the personOveride or the hub.getperson
-		if (missionGiver == null) return false;
-		
-		if (!setPersonMissionRef(missionGiver, "$vanidad_loe_ref")) {
-			return false;
-		}//put something in memory? its confusing
+        setTimeLimit(Stage.FAILED, 120, null);
+        setCreditReward(BaseHubMission.CreditReward.AVERAGE, 10);
 
-		targetObject = pickOne(	"a team operating as scout",
-					"a team collecting experimental data",
-					"a team establishing diplomatic contact",
-					"a travelling executive"
-							);
+        return true;
+    }
 
-		requireSystemTags(ReqMode.ANY,Tags.THEME_DERELICT, Tags.THEME_MISC, Tags.THEME_RUINS);
-		requireSystemTags(ReqMode.NOT_ANY, Tags.THEME_REMNANT); //remove remnant systems
-		preferSystemUnexplored();
-		preferSystemInDirectionOfOtherMissions();
-		
-		system = pickSystem();
-		if (system == null) return false;
-		
-                WeightedRandomPicker<Variation> variationPicker = new WeightedRandomPicker<Variation>(new Random());
-		variationPicker.add(Variation.NOTHING, 10f);
-		variationPicker.add(Variation.AUTHORITY, AUTHORITY_WEIGHTED_PROB);
-		variationPicker.add(Variation.SCAVENGER, SCAVENGER_WEIGHTED_PROB);
-                variation = variationPicker.pick();
-                if(rollProbability(SPACESHIP_PROBABILITY))
-                    targetVariation = TargetVariation.SPACESHIP;
-                else
-                    targetVariation = TargetVariation.PlANETSIDE;
-                variation = Variation.NOTHING;            
-                
-                
-                pickTargetShip();
-		if (entity == null) return false;
+    @Override
+    protected boolean callAction(String action, String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params,
+                                 Map<String, MemoryAPI> memoryMap) {
+        if (action.equals("clearedAuthority")) {
+            entity.getMemoryWithoutUpdate().unset(AUTHORITY_FLAG);
+            entity.getMemoryWithoutUpdate().unset(HAS_PROTECTING_FLEET_FLAG);
+            return true;
+        }
+        return false;
+    }
 
-		setStartingStage(Stage.GOGET);
-		setSuccessStage(Stage.COMPLETED);
-		addFailureStages(Stage.FAILED);
-                
-		makeImportant(entity, "$vanidad_loe_target", Stage.GOGET);
-		makeImportant(missionGiver, "$vanidad_loe_missionGiver", Stage.RETURN);
-		setEntityMissionRef(entity, "$vanidad_loe_ref");
-		
-		
-		connectWithMemoryFlag(Stage.GOGET, Stage.RETURN, entity, "$vanidad_loe_f_needToReturn");
-		setStageOnMemoryFlag(Stage.COMPLETED, missionGiver, "$vanidad_loe_f_completed");
-		
-                if (variation == Variation.SCAVENGER)
-                {
-                    
-                }
-                else if(variation == Variation.AUTHORITY)
-                {
-                    
-                }
-                
-		
-		setTimeLimit(Stage.FAILED, 120, null);
-		setCreditReward(BaseHubMission.CreditReward.AVERAGE, 10);
+    @Override
+    protected void updateInteractionDataImpl() {
 
-		
-		return true;
-	}
-        
-	@Override
-	protected void updateInteractionDataImpl() {
-		
-		set("$vanidad_loe_barEvent", isBarEvent());
-		set("$vanidad_loe_manOrWoman", missionGiver.getManOrWoman());
-                set("$vanidad_loe_missionGiverName", missionGiver.getNameString());
-		set("$vanidad_loe_reward", Misc.getWithDGS(getCreditsReward()));
-		
-		set("$vanidad_loe_target_system", system.getNameWithLowercaseTypeShort());
-		set("$vanidad_loe_target_text", targetObject);
-		set("$vanidad_loe_dist", getDistanceLY(system));
-                set("$vanidad_loe_targetNoArticle", getTargetWithoutArticle());
-                set("$vanidad_loe_variation", variation);
-	}
-	
-	@Override
-	public void addDescriptionForNonEndStage(TooltipMakerAPI info, float width, float height) {
-		float opad = 10f;
-		Color h = Misc.getHighlightColor();
-		if (currentStage == Stage.GOGET) {
-			info.addPara("Find the " + shipname + ", a " + hullname + 
-					 " ship " + getLocated(entity) + "." +
-                                         "Obtain black box of the pre-collapse VAO "+ getTargetWithoutArticle(), opad);
-				
-			
-		} else if (currentStage == Stage.RETURN) {
-			info.addPara(getReturnText(missionGiver.getMarket().getName()) + ".", opad);
-		}
-	}
+        set("$vanidad_loe_barEvent", isBarEvent());
+        set("$vanidad_loe_manOrWoman", missionGiver.getManOrWoman());
+        set("$vanidad_loe_missionGiverName", missionGiver.getNameString());
+        set("$vanidad_loe_reward", Misc.getWithDGS(getCreditsReward()));
+        set(BRIBE_VARIABLE, Misc.getWithDGS(getCreditsReward()/2));
 
-	@Override
+        set("$vanidad_loe_target_system", system.getNameWithLowercaseTypeShort());
+        set("$vanidad_loe_target_text", targetObject);
+        set("$vanidad_loe_dist", getDistanceLY(system));
+        set("$vanidad_loe_targetNoArticle", getTargetWithoutArticle());
+        set("$vanidad_loe_variation", variation);
+        if( FleetBlockingEntity != null ){
+            if (FleetBlockingEntity.getBattle() != null)
+                set("$vanidad_loe_protectingFleetIsBusy", true);
+            else
+                set("$vanidad_loe_protectingFleetIsBusy", false);
+        }
+    }
+
+    @Override
+    public void addDescriptionForNonEndStage(TooltipMakerAPI info, float width, float height) {
+        float opad = 10f;
+        Color h = Misc.getHighlightColor();
+        if (currentStage == Stage.GOGET) {
+            info.addPara("Find the " + shipname + ", a " + hullname
+                    + " ship " + getLocated(entity) + "."
+                    + "Obtain black box of the pre-collapse VAO " + getTargetWithoutArticle(),
+                         opad);
+
+        } else if (currentStage == Stage.RETURN) {
+            info.addPara(getReturnText(missionGiver.getMarket().getName()) + ".",
+                         opad);
+        }
+    }
+
+    @Override
     public boolean addNextStepText(TooltipMakerAPI info, Color tc, float pad) {
         Color h = Misc.getHighlightColor();
         if (currentStage == Stage.GOGET) {
@@ -203,62 +238,69 @@ public class vanidad_lostObject_mission extends HubMissionWithBarEvent {
         return false;
     }
 
-	@Override
-	public String getBaseName() {
-		return "Obtain VAO precollapse data ";
-	}
-        
+    @Override
+    public String getBaseName() {
+        return "Obtain VAO precollapse data ";
+    }
 
-    
-    private void pickTargetShip(){
-        	WeightedRandomPicker<DerelictShipEntityPlugin.DerelictType> picker = new WeightedRandomPicker<DerelictShipEntityPlugin.DerelictType>(new Random());
-		
-		picker.add(DerelictShipEntityPlugin.DerelictType.LARGE, 5f);
-		picker.add(DerelictShipEntityPlugin.DerelictType.MEDIUM, 10f);
-		picker.add(DerelictShipEntityPlugin.DerelictType.SMALL, 20f);
-                DerelictShipEntityPlugin.DerelictShipData shipData = DerelictShipEntityPlugin.createRandom("vanidad", picker.pick(), genRandom, DerelictShipEntityPlugin.getDefaultSModProb());
-                FactionAPI vanidadAPI = Global.getSector().getFaction("vanidad");
-                shipData.ship.shipName = vanidadAPI.pickRandomShipName();
-                shipname = shipData.ship.shipName;
-                String variantid = shipData.ship.variantId;
-                hullname = Global.getSettings().getVariant(variantid).getHullSpec().getHullNameWithDashClass();
-		entity = spawnDerelict(shipData, new LocData(EntityLocationType.HIDDEN, null, system, false));
-                entity.setDiscoverable(false);
+    private void pickTargetShip() {
+        WeightedRandomPicker<DerelictShipEntityPlugin.DerelictType> picker = new WeightedRandomPicker<DerelictShipEntityPlugin.DerelictType>(
+                new Random());
+
+        picker.add(DerelictShipEntityPlugin.DerelictType.LARGE, 5f);
+        picker.add(DerelictShipEntityPlugin.DerelictType.MEDIUM, 10f);
+        picker.add(DerelictShipEntityPlugin.DerelictType.SMALL, 20f);
+        DerelictShipEntityPlugin.DerelictShipData shipData = DerelictShipEntityPlugin.createRandom(
+                "vanidad", picker.pick(), genRandom,
+                DerelictShipEntityPlugin.getDefaultSModProb());
+        FactionAPI vanidadAPI = Global.getSector().getFaction("vanidad");
+        shipData.ship.shipName = vanidadAPI.pickRandomShipName();
+        shipname = shipData.ship.shipName;
+        String variantid = shipData.ship.variantId;
+        hullname = Global.getSettings().getVariant(variantid).getHullSpec().getHullNameWithDashClass();
+
+        entity = spawnDerelict(shipData, new LocData(EntityLocationType.ORBITING_PLANET_OR_STAR,
+                                                     null, system, true));
+        entity.setDiscoverable(false);
 
     }
-    
+
     protected String getTargetWithoutArticle() {
-		if (targetObject.startsWith("a ")) {
-			return targetObject.replaceFirst("a ", "");
-		}
-		if (targetObject.startsWith("an ")) {
-			return targetObject.replaceFirst("an ", "");
-		}
-		return targetObject;
-	}
-    
+        if (targetObject.startsWith("a ")) {
+            return targetObject.replaceFirst("a ", "");
+        }
+        if (targetObject.startsWith("an ")) {
+            return targetObject.replaceFirst("an ", "");
+        }
+        return targetObject;
+    }
+
     private void setupFleetEncounter() {
-            beginStageTrigger(Stage.GOGET);
+        beginStageTrigger(Stage.GOGET);
 
-            //fleet setup
-            triggerCreateFleet(FleetSize.LARGE, FleetQuality.DEFAULT,
-                               "vanidad", FleetTypes.PATROL_MEDIUM, entity.getLocation());
-            triggerAutoAdjustFleetStrengthModerate();
-            triggerSetFleetFaction("vanidad");
-            triggerMakeFleetIgnoredByOtherFleets();
-            triggerFleetSetName("Exploradors");
-            triggerMakeLowRepImpact();
+        //fleet setup
+        triggerCreateFleet(FleetSize.LARGE, FleetQuality.DEFAULT,
+                           "vanidad", FleetTypes.PATROL_MEDIUM,
+                           entity.getLocation());
+        triggerAutoAdjustFleetStrengthModerate();
+        triggerSetFleetFaction("vanidad");
+        triggerMakeFleetIgnoredByOtherFleets();
+        triggerFleetSetName("Exploradors");
+        triggerMakeLowRepImpact();
 
-            triggerPickLocationAroundEntity(entity, 5);
-            triggerSpawnFleetAtPickedLocation("$vanidad_loe_f_authority",
-                                              "$vanidad_loe_ref");
-            triggerOrderFleetInterceptPlayer();
-            
+        //triggerCreateLargePatrol(missionGiver.getMarket(), "vanidad", entity, Stage.GOGET, 1);
+        triggerPickLocationAroundEntity(entity, 5);
+        triggerSpawnFleetAtPickedLocation(AUTHORITY_FLAG,
+                                          "$vanidad_loe_ref");
+        triggerOrderFleetPatrol(entity);
+        triggerFleetOnlyEngageableWhenVisibleToPlayer();
+        triggerFleetSetPatrolLeashRange(100);
+        triggerFleetSetPatrolActionText("Orbiting derelict ship");
+        triggerFleetSetTravelActionText("Orbiting derelict ship");
+        triggerFleetAddDefeatTrigger(DEFENDING_FLEET_DEFEAT_TRIGGER);
+        triggerCustomAction(new ObtainFleetBlockingEntity());
+        //triggerOrderFleetInterceptPlayer();
+        endTrigger();
 
-            endTrigger();
-        
     }
 }
-
-
-
